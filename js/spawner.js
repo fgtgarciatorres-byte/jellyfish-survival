537eab550cf3b4e3c360d1c57c767af462dc76e1
fgtgarciatorres-byte <fgt.garciatorres@gmail.com>
2026-05-08 09:15:13 +0100

    Add enemy spawning logic in spawner.js

--- js/spawner.js
+++ js/spawner.js
@@ -1,1 +1,14 @@
+let spawnTimer = 0;
 
+function updateSpawner(){
+
+    spawnTimer -= gameState.delta;
+
+    if(spawnTimer <= 0){
+
+        spawnEnemy();
+
+        spawnTimer =
+        60 - gameState.level*2;
+    }
+}
