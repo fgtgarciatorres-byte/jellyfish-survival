2680ebfb51974a8926e5d6621e470076834257f0
fgtgarciatorres-byte <fgt.garciatorres@gmail.com>
2026-05-10 18:50:33 +0100

    Refactor GAME object structure in state.js

--- js/state.js
+++ js/state.js
@@ -1,13 +1,60 @@
 export const GAME = {
+
+    /* =========================
+       PANTALLA
+    ========================= */
+
     width: window.innerWidth,
     height: window.innerHeight,
 
+    /* =========================
+       ESTADO GENERAL
+    ========================= */
+
     running: true,
+
+    /* =========================
+       PROGRESIÓN
+    ========================= */
 
     score: 0,
     level: 1,
-   lives: 3,
+
+    /* =========================
+       VIDA
+    ========================= */
+
+    lives: 3,
+    maxLives: 3,
+
+    /* =========================
+       ENERGÍA
+    ========================= */
+
+    energy: 0,
+    maxEnergy: 100,
+
+    shooting: false,
+
+    /* =========================
+       INVENTARIO
+    ========================= */
+
+    diamonds: 0,
+    trophies: 0,
+
+    /* =========================
+       ESCUDO
+    ========================= */
+
+    shield: false,
+    shieldTimer: 0,
+
+    /* =========================
+       INPUT
+    ========================= */
 
     mouseX: window.innerWidth / 2,
     mouseY: window.innerHeight / 2
+
 };
